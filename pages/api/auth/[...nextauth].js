fc037facb3e19226b9324b2e426b1e781ae04f6e
meprazhant <meprazhant@gmail.com>
2023-07-07 18:23:30 +0545

    made admin login and all

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,14 @@ import GoogleProvider from "next-auth/providers/google"
 export const authOptions = {
     // Configure one or more authentication providers
     // secret
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    baseUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
     providers: [
         GoogleProvider({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
             clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
         }),
     ],
-    callbackUrl: '/auth',
+    secret: process.env.NEXT_PUBLIC_SECRET,
 }
 
 export default NextAuth(authOptions)
